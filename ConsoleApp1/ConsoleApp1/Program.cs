382f5ca8f3464b0da6f9cd01278ec7dac9775abe
serafima8 <147659413+serafima8@users.noreply.github.com>
2023-11-11 19:32:13 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //Задача 1. Составить программу, в результате которой величина а меняется значением с величиной b, а величина b — с величиной c.
+        //Определить метод, осуществляющий обмен значениями двух переменных величин.
+
+
+        // static void Main(string[] args)
+        // {
+        //     Console.Write("Введите величину a: ");
+        //     int a = Convert.ToInt32(Console.ReadLine());
+        //     Console.Write("Введите величину b: ");
+        //     int b = Convert.ToInt32(Console.ReadLine());
+        //     Console.Write("Введите величину c: ");
+        //     int c = Convert.ToInt32(Console.ReadLine());
+        //     IncrementVal(ref a, ref b, ref c);
+        //     Console.WriteLine($"a = {a}\t b = {b}\t c = {c}");
+        //     Console.ReadKey();
+        // }
+        // static void IncrementVal(ref int a, ref int b, ref int c)
+        //{
+        //     a = b;
+        //     b = c;
+        //}
+
+
+        //Задача 2. Ввести три целых числа и вывести их в порядке возрастания значений.
+        //static void Main(string[] args)
+
+
+        //{
+        //    Console.Write("Введите первое число: ");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите второе число: ");
+        //    int b = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("Введите третье число: ");
+        //    int c = Convert.ToInt32(Console.ReadLine());
+        //    IncrementVal(ref a, ref b, ref c);
+        //    Console.ReadKey();
+        //}
+        //static void IncrementVal(ref int a, ref int b, ref int c)
+        //{
+        //    int[] numbers = { a, b, c };
+        //    Array.Sort(numbers);
+        //    foreach (int number in numbers)
+        //    {
+        //        Console.WriteLine(number);
+        //    }
+        //}
+
+
+        // Задача 3. Создать метод, который будет вычислять площадь круга и длину окружности.
+        // Вызывающий программный код должен передавать в качестве аргумента методу значение радиуса и печатать значение радиуса, площади круга
+        // и длины окружности.
+
+        //public static double Square(double r)
+        //{
+        //    return ( Math.PI * Math.Pow(r, 2));
+        //}
+        //public static double Length(double r)
+        //{
+        //    return(2 * Math.PI * r);
+        //}
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("Введите радиус(r): ");
+        //    double r = Convert.ToDouble(Console.ReadLine());
+        //    Console.WriteLine($"Площадь круга = {Square(r)}");
+        //    Console.WriteLine($"Длина окружности = {Length(r)}");
+        //    Console.ReadKey();
+        //}
+
+
+
+
+    }
+}
